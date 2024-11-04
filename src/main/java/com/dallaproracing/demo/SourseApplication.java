cd485b36e9ab4ac1fe6f0f148989947e090e6ae9
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-04 14:52:32 -0300

    Projetc created

---
+++ src/main/java/com/dallaproracing/demo/SourseApplication.java
@@ -0,0 +1,13 @@
+package com.dallaproracing.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourseApplication.class, args);
+	}
+
+}
