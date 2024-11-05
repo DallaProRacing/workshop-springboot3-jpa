dc159fa05eff4fd065471f976be681f850669973
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-05 14:17:08 -0300

    Service layer, component registration

--- src/main/java/com/dallaproracing/demo/resources/UserResource.java
+++ src/main/java/com/dallaproracing/demo/resources/UserResource.java
@@ -1,20 +1,36 @@
 package com.dallaproracing.demo.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dallaproracing.demo.entities.User;
+import com.dallaproracing.demo.services.UserService;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
-
+	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Ruan", "ruan@gmail.com", "549898989", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> finById(@PathVariable long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
---
+++ src/main/java/com/dallaproracing/demo/services/UserService.java
@@ -0,0 +1,25 @@
+package com.dallaproracing.demo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.dallaproracing.demo.entities.User;
+import com.dallaproracing.demo.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
