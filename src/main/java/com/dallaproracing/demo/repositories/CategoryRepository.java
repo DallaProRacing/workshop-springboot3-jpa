6497fd5b2ce90712bd0bba3f728e50161e0b44cc
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-06 13:43:05 -0300

    Category

---
+++ src/main/java/com/dallaproracing/demo/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.dallaproracing.demo.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dallaproracing.demo.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+}
