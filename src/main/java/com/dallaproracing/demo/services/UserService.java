c68ed98037a9b9a92eca8fe4e0b3c3f8b1c95a16
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-07 15:28:19 -0300

    Exception handling - update

--- src/main/java/com/dallaproracing/demo/services/UserService.java
+++ src/main/java/com/dallaproracing/demo/services/UserService.java
@@ -13,6 +13,8 @@ import com.dallaproracing.demo.repositories.UserRepository;
 import com.dallaproracing.services.exceptions.DatabaseException;
 import com.dallaproracing.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -41,10 +43,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
-		
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 	private void updateData(User entity, User obj) {
 		
