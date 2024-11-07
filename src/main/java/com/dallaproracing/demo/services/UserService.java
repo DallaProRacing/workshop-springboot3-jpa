f08b2e47a720bb5f54b2530d242c0378281cae03
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-07 15:23:56 -0300

    Exception handling - findById

--- src/main/java/com/dallaproracing/demo/services/UserService.java
+++ src/main/java/com/dallaproracing/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.dallaproracing.demo.entities.User;
 import com.dallaproracing.demo.repositories.UserRepository;
+import com.dallaproracing.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -20,7 +21,7 @@ public class UserService {
 	}
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
