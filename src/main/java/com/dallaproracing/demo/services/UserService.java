ceee8eacfbd47ce0cff2fcbb782c470c9ab234ab
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-07 15:27:35 -0300

    Exception handling - delete

--- src/main/java/com/dallaproracing/demo/services/UserService.java
+++ src/main/java/com/dallaproracing/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.dallaproracing.demo.entities.User;
 import com.dallaproracing.demo.repositories.UserRepository;
+import com.dallaproracing.services.exceptions.DatabaseException;
 import com.dallaproracing.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -28,7 +31,13 @@ public class UserService {
 		return repository.save(obj);
 	}
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
